dafe15d0e19ce7a63cc05521f17c0d56db10f037
Dalaryizmi <vang12383@gmail.com>
2020-04-23 23:51:03 +0700

    Change ID

--- app/src/main/java/vn/edu/ntu/vosonvang/vosonvang_59133093_simplewidget/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/vosonvang/vosonvang_59133093_simplewidget/MainActivity.java
@@ -3,12 +3,26 @@ package vn.edu.ntu.vosonvang.vosonvang_59133093_simplewidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnConfirm = findViewById(R.id.btnConfirm);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "...",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
